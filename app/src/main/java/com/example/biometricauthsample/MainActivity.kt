113c31936421a561ddf5cae778b65d40be45af97
aaismailov <ismailovasados@mail.ru>
2022-10-22 01:30:10 +0300

    EncryptedSharedPreferences local saving with crypto

--- app/src/main/java/com/example/biometricauthsample/MainActivity.kt
+++ app/src/main/java/com/example/biometricauthsample/MainActivity.kt
@@ -12,12 +12,31 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.FragmentActivity
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 import com.example.biometricauthsample.ui.theme.BiometricAuthSampleTheme
+
 
 class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            "secret_shared_prefs",
+            masterKeyAlias,
+            this,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+        sharedPreferences.edit().apply {
+            putString("user.name", "Joseph Artega")
+            putString("user.pin", "1450")
+        }.apply()
+
         setContent {
             val context = LocalContext.current
             BiometricAuthSampleTheme {
@@ -66,10 +85,22 @@ class MainActivity : FragmentActivity() {
                                         description = "Authentication is must",
                                         negativeText = "Cancel",
                                         onSuccess = {
+                                            var userName =
+                                                sharedPreferences.getString("user.name", "")
+                                            var userPin =
+                                                sharedPreferences.getString("user.pin", "")
+
                                             runOnUiThread {
                                                 Toast.makeText(
                                                     context,
                                                     "Authenticated successfully",
+                                                    Toast.LENGTH_SHORT
+                                                )
+                                                    .show()
+
+                                                Toast.makeText(
+                                                    context,
+                                                    "$userName : $userPin",
                                                     Toast.LENGTH_SHORT
                                                 )
                                                     .show()
